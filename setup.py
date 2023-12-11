08188f75d3238835b95614ec13328f5558dac222
Paulo Musachio <33529986+pmusachio@users.noreply.github.com>
2023-12-11 19:02:14 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helper_functions',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Lista de suas dependências aqui
+    ],
+)
